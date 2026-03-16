901b6fa513a82ad5b9f81b119d22c1c20eb6f646
agonzalezbridge <asier.medina.ugalde@gmail.com>
2026-03-16 15:42:13 +0100

    .

--- src/models/Space.js
+++ src/models/Space.js
@@ -1,11 +1,11 @@
 export default class Space {
 
-  constructor(id, name, users = [], tasks = [], createdAt,ownerId) {
+  constructor(id, name, users = [], tasks = [], createDate,ownerId) {
     this.id = id;
     this.name = name;
     this.users = users;
     this.tasks = tasks;
-    this.createdAt= createdAt;
+    this.createDate= createDate;
     this.ownerId= ownerId ;
   }
 
--- src/models/Task.js
+++ src/models/Task.js
@@ -1,11 +1,11 @@
 export default class Task {
 
-  constructor(id, title,description, difficulty,createdAt, assignedTo = null,dueDate= null, completed = false) {
+  constructor(id, title,description, difficulty,createDate, assignedTo = null,dueDate= null, completed = false) {
     this.id = id;
     this.title = title;
     this.description = description;
     this.difficulty = difficulty; // easy, medium, hard
-    this.createdAt=createdAt;
+    this.createDate=createDate;
     this.assignedTo = assignedTo;
     this.dueDate =dueDate;
     this.completed = completed;
