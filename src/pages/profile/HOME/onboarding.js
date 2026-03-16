f0612c0e8f29b360a292fda76a0d0bffea7b78ca
jefferson <jeffersonffers@hmail.com>
2026-03-16 16:03:38 +0100

    De nuevo

--- src/pages/profile/HOME/onboarding.js
+++ src/pages/profile/HOME/onboarding.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const createModal = document.getElementById("createModal")
-    const joinModal = document.getElementById("joinModal")
+/* USUARIO*/
 
     const user = JSON.parse(localStorage.getItem("usuarioActual"))
 
     if(!user){
-        window.location.href = "../auth/auth.html"
+        window.location.href = "../../auth/auth.html"
         return
     }
 
@@ -14,11 +13,61 @@ document.addEventListener("DOMContentLoaded", () => {
     const avatar = document.getElementById("avatar")
 
     if(welcomeName){
-        welcomeName.innerText = "Bienvenido, " + user.name
+        welcomeName.innerText = "¡Bienvenido, " + user.name + "!"
     }
 
     if(avatar){
         avatar.src = "https://api.dicebear.com/7.x/adventurer/svg?seed=" + user.name
     }
 
+/* HEADER*/
+
+    fetch("../../../components/header/header.html")
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("header").innerHTML = data
+    })
+
+/* FOOTER */
+
+    fetch("../../../components/footer/footer.html")
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("footer").innerHTML = data
+    })
+
+/* MODAL CREAR ESPACIO */
+
+    fetch("../../../components/modals/create-space.html")
+    .then(res => res.text())
+    .then(data => {
+        document.body.insertAdjacentHTML("beforeend", data)
+    })
+
+
+/* MODAL UNIRSE*/
+
+    fetch("../../../components/modals/join-space.html")
+    .then(res => res.text())
+    .then(data => {
+        document.body.insertAdjacentHTML("beforeend", data)
+    })
+
 })
+
+/* ABRIR MODALES */
+
+function openCreate(){
+    document.getElementById("createModal").style.display = "flex"
+}
+
+function openJoin(){
+    document.getElementById("joinModal").style.display = "flex"
+}
+
+
+/* CERRAR MODAL */
+
+function closeModal(id){
+    document.getElementById(id).style.display = "none"
+}
