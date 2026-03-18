a517205dd4e55a556b2685124f3d6ed9f29d3f98
Txe <labiennacida@gmail.com>
2026-03-18 11:53:16 +0100

    Registro y login funcionando otra vez

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -1,11 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     let modo = "login"
-    let modo = "login"
 
-    const emailInput = document.getElementById("email")
-    const passwordInput = document.getElementById("password")
-    const nameField = document.getElementById("nameField")
     const emailInput = document.getElementById("email")
     const passwordInput = document.getElementById("password")
     const nameField = document.getElementById("nameField")
