66397b917bc1288615155a6efc60f2ee519bb1a3
Txe <labiennacida@gmail.com>
2026-03-18 12:05:57 +0100

    Actualizada la rama

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -87,3 +87,56 @@ document.addEventListener("DOMContentLoaded", () => {
         .getElementById("registerLink")
         ?.addEventListener("click", mostrarRegistro)
 })
+
+// ======================
+// LOGIN
+// ======================
+window.loginUser = function (email, password) {
+
+    if (!email || !password) {
+        return { ok: false, message: "Rellena todos los campos" }
+    }
+
+    let user = JSON.parse(localStorage.getItem(email))
+
+    if (!user) {
+        return { ok: false, message: "Usuario no encontrado" }
+    }
+
+    if (user.password !== password) {
+        return { ok: false, message: "Contraseña incorrecta" }
+    }
+
+    localStorage.setItem("usuarioActual", JSON.stringify(user))
+
+    return { ok: true }
+}
+
+// ======================
+// REGISTRO
+// ======================
+window.registerUser = function (name, email, password) {
+
+    if (!name || !email || !password) {
+        return { ok: false, message: "Rellena todos los campos" }
+    }
+
+    let existe = localStorage.getItem(email)
+
+    if (existe) {
+        return { ok: false, message: "El usuario ya existe" }
+    }
+
+    let user = {
+        name,
+        email,
+        password
+    }
+
+    localStorage.setItem(email, JSON.stringify(user))
+    localStorage.setItem("usuarioActual", JSON.stringify(user))
+
+    return { ok: true }
+}
+
+
